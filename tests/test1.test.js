cf84d4578d30c98cc2f6f41be74d72b386cc1db6
malkarot <malkarot92@gmail.com>
2023-05-01 00:01:40 +0300

    commit from vs

--- tests/test1.test.js
+++ tests/test1.test.js
@@ -5,12 +5,12 @@ describe("Test suite 1:", ()=>{
     test("test 1: ", ()=>{
         expect(2).toEqual(2)
     })
-    // test("test 2: ", async ()=>{
-    //     const res = await request(app).get('./')
-    //     expect(res.statusCode).toEqual(200)
-    // })
+    test("test 2: ", async ()=>{
+        const res = await request(app).get('./')
+        expect(res.statusCode).toEqual(200)
+    })
     test("test 3: ", async()=>{
         const res = await request(app).get('./1222')
-        expect(res.statusCode).toEqual(400)
+        expect(res.statusCode).toEqual(404)
     })
 })
